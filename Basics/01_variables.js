0a8ec989c18c4722bfbde87c91a18bae5cd02971
Aaryan2332 <aarucode2332@gmail.com>
2026-08-20 03:10:30 +0000

    variables

---
+++ Basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 1243
+let accountEmail = "aaru@gmail.com"
+var accountPass = "123"
+accountCity = "ludhiana"
+
+accountEmail = "haauu@gmail.com"
+accountPass = "4835483"
+accountCity = "Mohali"
+
+console.log(accountId);
+console.table([accountId , accountEmail, accountPass , accountCity])
