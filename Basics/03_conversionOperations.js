ca296ee584dc2bc314ff87c4330d6294704d6b5a
Aaryan2332 <aarucode2332@gmail.com>
2026-08-21 06:05:48 +0000

    conversion of datatypes

---
+++ Basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "46abs" => NaN(not a number)
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=>true; 0=>false
+// "" => false
+// "name" => true
+
+let someNum = 33
+
+let stringNumber = String(someNum)
+console.log(stringNumber)
+console.log(typeof stringNumber)
