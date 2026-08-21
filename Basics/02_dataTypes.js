93adcd5d79240a9fd0690c6c5b8e54df18916aea
Aaryan2332 <aarucode2332@gmail.com>
2026-08-21 03:36:05 +0000

    datatypes

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+ let name  = "Aaryan"
+ let age = 18
+ let isLoggedIn = false
+
+// -----DataTypes-----
+// number => 2 to power 53
+// bigint
+// string
+// boolean
+// null => it is a stand alone value
+// undefined => value is not defined yet
+// symbol 
+
+
+// object
+
+console.log(typeof age);
+console.log(typeof name);
+console.log(typeof null);//it is a object
+
+
+
