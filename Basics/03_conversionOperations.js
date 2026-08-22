06b5cbf02dbbf8b21f99a3c5585fdba8fe5ffe32
Aaryan2332 <aarucode2332@gmail.com>
2026-08-22 07:42:08 +0000

    comparisions

--- Basics/03_conversionOperations.js
+++ Basics/03_conversionOperations.js
@@ -24,3 +24,21 @@ let someNum = 33
 let stringNumber = String(someNum)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Aaryan"
+let str3 = str1+str2
+console.log(str3); 
+
+// Prefix and Postfix operators
+let gameCount = 1000
+gameCount++
+console.log(gameCount);
+
+let gameCount2 = 2000
+++gameCount2
+console.log(gameCount2);
