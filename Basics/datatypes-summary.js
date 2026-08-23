2407c2746d5e6e7c34ed8560271c4f4ec005e72a
Aaryan2332 <aarucode2332@gmail.com>
2026-08-23 04:43:57 +0000

    changes

--- Basics/datatypes-summary.js
+++ Basics/datatypes-summary.js
@@ -27,3 +27,29 @@ console.log(typeof myFunction);
 console.log(typeof cars);
 console.log(typeof myObj);
 console.log(typeof id);
+
+
+// (STACK & HEAP)
+
+// Stack is used in Primitive Datatypes
+// heap is used in Non - Primitive Datatypes
+let myName = "aman"
+
+let anotherName = myName
+anotherName = "Aaryan"
+
+console.log(myName);
+console.log(anotherName);
+
+let userOne = {
+    email : "userone.com",
+    upi : "fiuvn"
+
+}
+
+let userTwo = userOne 
+
+userTwo.email = "jdcncwnc"
+
+console.log(userOne.email);
+console.log(userTwo.email);
