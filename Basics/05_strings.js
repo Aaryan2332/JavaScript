158c8e68c44c5801d26b416ad630cb8ce033bbcf
Aaryan2332 <aarucode2332@gmail.com>
2026-08-27 02:09:28 +0000

    strings

---
+++ Basics/05_strings.js
@@ -0,0 +1,32 @@
+const name = "Aaryan"
+const repos = 5
+
+console.log(`Hello my name is ${name} and my repo count is ${repos}`);
+
+const gameName = new String("Free-Fire")
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(0));
+console.log(gameName.indexOf('e'));
+
+const newString = gameName.substring(0,5)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   aaryan   "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+console.log(newStringOne.trimStart());
+console.log(newStringOne.trimEnd());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20' , '-'));
+console.log(url.includes('hitesh'));
+
+console.log(gameName.split('-'));
