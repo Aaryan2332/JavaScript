82694eebdfc91edc45fd65f2c19b676f84435d37
Aaryan2332 <aarucode2332@gmail.com>
2026-08-29 11:06:49 +0000

    dates

---
+++ Basics/07_dates.js
@@ -0,0 +1,26 @@
+// Dates 
+
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(1999 , 4 , 17)
+let myCreatedDate = new Date("2008-01-15")
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate  = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDay());
+
