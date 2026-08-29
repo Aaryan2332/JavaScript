2334cc798e5bb8f9a87841d3a44142c7693b7776
Aaryan2332 <aarucode2332@gmail.com>
2026-08-29 11:06:28 +0000

    maths

---
+++ Basics/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance); 
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNum = 88.7859
+// console.log(otherNum.toPrecision(3));
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// -------------------- Maths ------------------- 
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(78.934957));
+// console.log(Math.ceil(78.934957));
+// console.log(Math.floor(78.934957));
+// console.log(Math.min(23,45,1,67));
+// console.log(Math.max(23,45,1,67));
+console.log(Math.random());
+
+const  min = 10
+const  max = 30
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min);
