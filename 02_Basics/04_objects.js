956d2ce3f429330ec6d1f1f0dfc9109442eda215
Aaryan2332 <aarucode2332@gmail.com>
2026-08-31 19:49:58 +0000

    objects

---
+++ 02_Basics/04_objects.js
@@ -0,0 +1,34 @@
+const tinderUser = new Object()
+tinderUser.id = "7340980"
+tinderUser.name = "Aman"
+tinderUser.isloggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email : "some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"aaryan",
+            lastname : "sharma"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+
+// const obj3 = Object.assign(obj1,obj2)
+
+const obj3 = {...obj1, ...obj2}
+console.log(obj3);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+
+
