a29e7dd4047fb3a3b4cb2027e700be9e7af221fa
Aaryan2332 <aarucode2332@gmail.com>
2026-09-01 16:12:20 +0000

    jason

--- 02_Basics/04_objects.js
+++ 02_Basics/04_objects.js
@@ -24,11 +24,26 @@ const obj2 = {3: "a", 4: "b"}
 // const obj3 = Object.assign(obj1,obj2)
 
 const obj3 = {...obj1, ...obj2}
-console.log(obj3);
+// console.log(obj3);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+const car = {
+    carName: "Polo gt",
+    carModel: "2020",
+    carPrice: "800000"
+
+}
+
+const {carName} = car 
+console.log(carName);
 
 
 
+// jason form
+// {
+//     "name" : "aaryan",
+//     "age": "18"
+// }
