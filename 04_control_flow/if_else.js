626b64c6d256c7df3b971850c56413344f2d22e2
Aaryan2332 <aarucode2332@gmail.com>
2026-09-09 16:00:34 +0000

    if_else

---
+++ 04_control_flow/if_else.js
@@ -0,0 +1,52 @@
+// if 
+const temp = 30
+
+// if(temp == 29){
+//     console.log("temp given is correct");
+    
+// }else{
+//     console.log("temp given is not correct");
+    
+// }
+
+// const score = 2000
+
+// if(score > 1000){
+//     const power = "swim"
+//     console.log(`user power: ${power}`);
+    
+// }
+
+// console.log(`user power: ${power}`);
+
+const balance = 1000
+
+// if (balance > 500) console.log("test");
+
+// if(balance < 500){
+//     console.log("less than 500");
+// }else if(balance < 750){
+//     console.log("less than 750");
+
+// }else if(balance < 999){
+//     console.log("less than 999");
+    
+// }else{
+//     console.log("less than 1001");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard){
+    console.log("Allow to shop courses");
+    
+}
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in");
+    
+}
