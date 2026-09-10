d45febf2e41b65cb61140a86bd3ddeb202fc59c5
Aaryan2332 <aarucode2332@gmail.com>
2026-09-10 05:26:04 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,27 @@
+const month = 2
+
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+    case 4:
+        console.log("Apr");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("june");
+        break;
+
+    default:
+        console.log("kuch nahi");
+        
+        break;
+} 
