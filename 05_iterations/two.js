1963393c8a43ae56f5e71f00b33c74584713fa57
Aaryan2332 <aarucode2332@gmail.com>
2026-09-13 17:14:48 +0000

    while/do-while

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// while loops 
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index += 2;
+// }
+
+let myArr = ["spiderman", "vision", "Iron-man"]
+
+// let arr = 0
+// while (arr<myArr.length) {
+//     console.log(`Hero is ${myArr[arr]}`);
+//     arr++;
+// }
+
+// Do - while loops
+
+
+let score = 11
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<=10);
