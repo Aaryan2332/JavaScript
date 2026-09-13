deab2275f17ce43f5b643b71ebec34419b5ffea9
Aaryan2332 <aarucode2332@gmail.com>
2026-09-13 17:03:37 +0000

    for loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,51 @@
+// for
+
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     if (element == 6) {
+//         console.log("6 is the chakka");
+        
+//     }
+//     console.log(element);   
+// }
+
+// // for (let i = 1; i <= 10; i++) {
+//     console.log(`Outer Loop value: ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         // console.log(`Inner loop value: ${j} and inner loop ${i}`);
+//         console.log(i + '*' + j + ' = ' + i*j);
+        
+//     }
+    
+// }
+
+// let myArr = ["flash" , "batman", "superman"]
+// console.log(myArr.length);
+
+// for (let index = 0; index < myArr.length; index++) {
+//     const element = myArr[index];
+//     console.log(element);
+    
+// }
+
+// Break and Continue
+
+// for (let index = 1; index < 20; index++) {
+//     if(index == 5){
+//         console.log(`detected 5`);
+//         break;
+//     }    
+//     console.log(`value of i is ${index}`);
+            
+// }
+
+
+for (let index = 1; index < 20; index++) {
+    if(index == 5){
+        console.log(`detected 5`);
+        continue;
+    }    
+    console.log(`value of i is ${index}`);
+            
+}
