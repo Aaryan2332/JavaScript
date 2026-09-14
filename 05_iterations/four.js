7e0912d0483157dd87c8a883d48c8d398797d1b0
Aaryan2332 <aarucode2332@gmail.com>
2026-09-14 16:55:34 +0000

    forin_loops

---
+++ 05_iterations/four.js
@@ -0,0 +1,16 @@
+// const myObj = {
+//     js: 'javascript',
+//     cpp: 'C++',
+//     py: 'python'
+// }
+
+// for (const key in myObj) {
+//     console.log(`${key} shortcut is for ${myObj[key]}`);
+// }  
+
+const programming  = ["js" , "py", "c++", "java"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+        
+}
